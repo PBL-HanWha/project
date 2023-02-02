66c579525241ffbf05f0ebc2d3991158be3c2ebc
BAEK9734 <butcheriron9734@gmail.com>
2023-02-02 15:52:33 +0900

    log.txt생성

---
+++ txtsave.py
@@ -0,0 +1,24 @@
+import datetime
+
+name = 'ship'
+x = 10
+y = 20
+
+
+f = open('log.txt','a')
+
+msg = [str(datetime.datetime.now()),': [',str(name),'] is detected... Location : X = ',str(x),', Y = ',str(y),'\n']
+msg = ''.join(msg)
+f.write(msg)
+f.close 
+
+
+name2 = 'airplane'
+x2 = 11
+y2 = 22
+
+f = open('log.txt','a')
+msg = [str(datetime.datetime.now()),': [',str(name),'] is detected... Location : X = ',str(x),', Y = ',str(y),'\n']
+msg = ''.join(msg)
+f.write(msg)
+f.close 
